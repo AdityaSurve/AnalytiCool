f132b290ee622187205b73da44a8916eb5526dd5
AdityaSurve <surveaditya521@gmail.com>
2023-05-10 17:17:23 +0530

    dashboard_updated

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -12,4 +12,12 @@ router.get("/kpis", async (req, res) => {
   }
 });
 
+router.get("/kpis", async (req, res) => {
+  try {
+    const kpis = await KPI.find();
+    res.status(200).json(kpis);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 export default router;
